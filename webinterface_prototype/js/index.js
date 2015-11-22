d13a8266964352c28d9f6fd75884f7c0341dde9f
Julian Haluska <s9juhalu@stud.uni-saarland.de>
2015-11-22 20:44:38 +0100

    This are the HTML, CSS and bootstrap files for the first prototype of the webinterface.

---
+++ webinterface_prototype/js/index.js
@@ -0,0 +1,4 @@
+function show_visualization(id_visualization_result, id_query) {
+  document.getElementById(id_visualization_result).style.display = "block";
+  document.getElementById(id_query).style.padding = "35px";
+}
