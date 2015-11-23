0c3e5d86f6aaa49b5ed4c5c24b0d583d44bcc1a5
Julian Haluska <s9juhalu@stud.uni-saarland.de>
2015-11-23 15:45:54 +0100

    New version of the webinterface including a 'Change Model' site. Also there are now checkboxes near the input fields and there are now two input fields - one for a verb and one for a noun. The select element is now for the selection of a role. Added 'WSVT' above the query as a placeholder for a logo or a name of the tool. Now the starting site does conatin a preloaded image, the version without the preloaded visualization does not exist anymore. Implemented a javascript function which lets only one of the checkboxes to be checked.

--- webinterface_prototype/js/index.js
+++ webinterface_prototype/js/index.js
@@ -1,4 +1,23 @@
-function show_visualization(id_visualization_result, id_query) {
-  document.getElementById(id_visualization_result).style.display = "block";
-  document.getElementById(id_query).style.padding = "35px";
+function checkbox_constraint(this_checkbox) {
+  this_checkbox_checked = document.getElementById(this_checkbox).checked;
+  if (this_checkbox_checked) {
+    if (this_checkbox == "checkbox_noun") {
+      document.getElementById("checkbox_verb").disabled = true;
+      document.getElementById("checkbox_noun").checked = true;
+    }
+    else if (this_checkbox == "checkbox_verb") {
+      document.getElementById("checkbox_noun").disabled = true;
+      document.getElementById("checkbox_verb").checked = true;
+    }
+  }
+  else {
+    if (this_checkbox == "checkbox_noun") {
+      document.getElementById("checkbox_verb").disabled = false;
+      document.getElementById("checkbox_noun").checked = false;
+    }
+    else if (this_checkbox == "checkbox_verb") {
+      document.getElementById("checkbox_noun").disabled = false;
+      document.getElementById("checkbox_verb").checked = false;
+    }
+  }
 }
