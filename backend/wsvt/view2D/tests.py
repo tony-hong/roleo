61351b31f4ffd7a2eea6f986e73a2255c2ca016c
Tony Hong <humanhong1001@gmail.com>
2016-01-02 15:00:23 +0100

    Add a sample of writing unit tests for view2D

--- backend/wsvt/view2D/tests.py
+++ backend/wsvt/view2D/tests.py
@@ -1,3 +1,10 @@
+from django.core.urlresolver import resolve
 from django.test import TestCase
+from view2D.views import *
 
-# Create your tests here.
+class IndexTest(TestCase):
+    def test_URLResolvesToIndexView(self):
+        found = resolve('/view2D')
+        self.assertEqual(found.func, index)
+
+# TODO add other test
