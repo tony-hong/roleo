37d335e9f58133a70003fbf5de3230140e5a2c03
Tony Hong <humanhong1001@gmail.com>
2016-01-02 11:24:22 +0100

    Add a sample of writing functional tests

--- backend/test/functional_tests.py
+++ backend/test/functional_tests.py
@@ -1,7 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000/view2D')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Web-based semantic visualization tool' in browser.title
+    # TODO: fill in the name of the use case
+    def test_nameOfUC(self):
+        # Open the browser and visit the tool
+        self.browser.get('http://localhost:8000/view2D')
+        # Wait for 3 secs
+        self.browser.implicitly_wait(3)
+        
+        # TODO: write the statements and exceptions in the use case here
+        self.assertIn('Web-based semantic visualization tool', self.browser.title)
+        self.fail('Finish the test!')
+
+    # TODO: define more tests
+    # ...
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main()
