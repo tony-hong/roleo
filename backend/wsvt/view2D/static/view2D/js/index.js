8df61354d2c2f0badd7689596bcc50e84f2e45c4
Julian Haluska <s9juhalu@stud.uni-saarland.de>
2016-02-09 20:21:35 +0100

    Now the code is better structured from a layout perspective and so better readable and commented

--- backend/wsvt/view2D/static/view2D/js/index.js
+++ backend/wsvt/view2D/static/view2D/js/index.js
@@ -1,38 +1,46 @@
 // this function starts the presentation mode of the query site
 function start_presentation_mode() {
+
   // disable nav bar at the top
   document.getElementById("navigation_bar").style.display = "none";
+
   // disable menu bar on the left
   document.getElementById("left_menu_bar").style.display = "none";
+
   // make the End Presentation Mode button visible
   document.getElementById("end_presentation_mode").style.display = "inline";
+
   // make the Start Presentation Mode button invisible
   document.getElementById("start_presentation_mode").style.display = "none";
+
   // set body padding-top: 0px;
   document.body.style.paddingTop = "0px";
-  // change the column classes
+
+  // change the column classes, so that the columns which are still visible, use the new space, which the left columns leaves
   document.getElementById("canvas_column").className = "col-md-8 col-sm-7 col-xs-12 col-ld-8";
   document.getElementById("right_menu_column").className = "col-md-4 col-sm-5 col-xs-12 col-ld-4";
-
-  // make canvas bigger
 
 }
 
 // this function ends the presentation mode of the query site
 function end_presentation_mode() {
+
   // enable nav bar at the top
   document.getElementById("navigation_bar").style.display = "inline";
+
   // enable menu bar on the left
   document.getElementById("left_menu_bar").style.display = "inline";
+
   // make the End Presentation Mode button invisible again
   document.getElementById("end_presentation_mode").style.display = "none";
+
   // make the Start Presentation Mode button visible again
   document.getElementById("start_presentation_mode").style.display = "inline";
+
   // set body padding-top: 50px;
   document.body.style.paddingTop = "50px";
-  // change the column classes back
-  document.getElementById("canvas_column").className = "col-md-7 col-sm-6 col-xs-12 col-ld-6";
-  document.getElementById("right_menu_column").className = "col-md-3 col-sm-3 col-xs-12 col-ld-3";
 
-  // make canvas smaller
+  // change the column classes back, so that the columns do not use the space of the left column anymore
+  document.getElementById("canvas_column").className = "col-md-7 col-sm-7 col-xs-12 col-ld-6";
+  document.getElementById("right_menu_column").className = "col-md-3 col-sm-2 col-xs-12 col-ld-3";
 }
