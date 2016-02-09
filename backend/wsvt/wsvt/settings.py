bf2ce43e0b746d6465e775e511e6169cc728d75b
Tony Hong <humanhong1001@gmail.com>
2016-02-09 11:44:46 +0100

    Modify the deployment path of static files

--- backend/wsvt/wsvt/settings.py
+++ backend/wsvt/wsvt/settings.py
@@ -99,6 +99,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static/")
 
 STATIC_URL = '/static/'
