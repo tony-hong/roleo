0c7e54d0342e25f2f733c723373afd8500d4eb83
Tony Hong <humanhong1001@gmail.com>
2016-02-09 11:47:43 +0100

    Feature #5879: Add a mapping from machine labeled roles to human readable labels

--- backend/wsvt/view2D/models.py
+++ backend/wsvt/view2D/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 class SemanticRole(models.Model):
     name = models.CharField(max_length=10)
+    description = models.CharField(default = '', max_length=64)
     def __unicode__(self):              # __unicode__ on Python 2
         return self.name
+
+# class SemanticModel(models.Model):
+#     name = models.CharField(max_length=10)
+#     description = models.CharField(default = '', max_length=256)
+#     def __unicode__(self):              # __unicode__ on Python 2
+#         return self.name
