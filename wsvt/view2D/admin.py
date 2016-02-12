a85feb4ace9808fe9207932fc4ba5a2c1a19a710
Tony Hong <humanhong1001@gmail.com>
2016-02-12 21:59:30 +0100

    Add hook for change model in admin.py

--- wsvt/view2D/admin.py
+++ wsvt/view2D/admin.py
@@ -1,5 +1,16 @@
+'''
+    This module is used to configure the SemanticRole in models in admin page, 
+    so that the admin can configure the value of SemanticRole in admin page
+
+    @Author: 
+        Tony Hong
+'''
 from django.contrib import admin
 
 from .models import SemanticRole
+# from .models import SemanticModel
 
 admin.site.register(SemanticRole)
+
+# TODO: The hook for change model function
+# admin.site.register(SemanticModel)
