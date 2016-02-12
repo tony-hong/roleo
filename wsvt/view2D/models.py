d0b2547d176e8637e51a9f53ae91e7bb189492af
Tony Hong <humanhong1001@gmail.com>
2016-02-12 22:24:16 +0100

    Add more comments for the models(MVC) in view2D app

--- wsvt/view2D/models.py
+++ wsvt/view2D/models.py
@@ -1,3 +1,10 @@
+'''
+    This module contains the models of view2D app.
+    All models are associated to a schema in database.
+
+    @Author: 
+        Tony Hong
+'''
 from django.db import models
 
 class SemanticRole(models.Model):
@@ -6,6 +13,7 @@ class SemanticRole(models.Model):
     def __unicode__(self):              # __unicode__ on Python 2
         return self.name
 
+# TODO: hook for 'change semantic model'
 # class SemanticModel(models.Model):
 #     name = models.CharField(max_length=10)
 #     description = models.CharField(default = '', max_length=256)
