c54e377706d37cb12465db0db42d95702e109f34
Tony Hong <humanhong1001@gmail.com>
2016-02-12 16:46:39 +0100

    Feature #3791: Modify readable messages for errors which show up to client

--- wsvt/view2D/errorCodeJSON.py
+++ wsvt/view2D/errorCodeJSON.py
@@ -1,13 +1,13 @@
 import errorCode
 
 errorCodeJSON = {
-    errorCode.NOUN_FORMAT_ERROR : "exception: noun format error",
-    errorCode.VERB_FORMAT_ERROR : "exception: verb format error",
-    errorCode.NOUN_EMPTY        : "exception: noun is empty",
-    errorCode.VERB_EMPTY        : "exception: verb is empty",
-    errorCode.MBR_VEC_EMPTY     : "exception: memberVectors is empty, main query word / semantic role does not exist",
-    errorCode.QUERY_EMPTY       : "exception: query is empty, query word does not exist",
-    errorCode.SMT_ROLE_EMPTY    : "exception: query.ix[semanticRole] is empty, / semantic role of query word does not exist",
-    errorCode.INTERNAL_ERROR    : "exception: internal error",
-    errorCode.NON_ENGLISH       : "exception: non english word is temporarily not supported"
+    errorCode.NOUN_FORMAT_ERROR : "Noun format error",
+    errorCode.VERB_FORMAT_ERROR : "Verb format error",
+    errorCode.NOUN_EMPTY        : "Noun as primal query word is empty",
+    errorCode.VERB_EMPTY        : "Verb as primal query word is empty",
+    errorCode.MBR_VEC_EMPTY     : "The model return nothing for the primal query word", # MemberVectors is empty, main query word / 
+    errorCode.QUERY_EMPTY       : "Second query word does not exist in the model",      # query is empty, 
+    errorCode.SMT_ROLE_EMPTY    : "Semantic role of second query word does not exist",  # query.ix[semanticRole] is empty, / 
+    errorCode.INTERNAL_ERROR    : "Server internal error, please refresh the page",
+    errorCode.NON_ENGLISH       : "Non english word is temporarily not supported"
 }
