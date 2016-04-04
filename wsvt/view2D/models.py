c3c1e223d23bcc3f954abc7dec8cfde9dabc3338
Tony Hong <humanhong1001@gmail.com>
2016-04-04 11:29:23 +0200

    Implement new model for Changing Model

--- wsvt/view2D/models.py
+++ wsvt/view2D/models.py
@@ -8,14 +8,10 @@
 from django.db import models
 
 class SemanticRole(models.Model):
-    name = models.CharField(max_length=10)
-    description = models.CharField(default = '', max_length=64)
+    labelSDDM = models.CharField(default = 'null', max_length=32)
+    labelTypeDM = models.CharField(default = 'null', max_length=32)
+    name = models.CharField(default = 'null', max_length=32)
+    # 1: SDDM, 2: both, 3: TypeDM
+    modelSupport = models.IntegerField(default = 1)
     def __unicode__(self):              # __unicode__ on Python 2
         return self.name
-
-# TODO: hook for 'change semantic model'
-# class SemanticModel(models.Model):
-#     name = models.CharField(max_length=10)
-#     description = models.CharField(default = '', max_length=256)
-#     def __unicode__(self):              # __unicode__ on Python 2
-#         return self.name
