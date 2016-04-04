99af9ecb3084971c571b0241f748452fdefb25f2
Tony Hong <humanhong1001@gmail.com>
2016-04-04 13:45:02 +0200

    Add currentModel var in MatrixFactory

--- wsvt/view2D/matrixFactory.py
+++ wsvt/view2D/matrixFactory.py
@@ -10,37 +10,41 @@ os.system('export LD_LIBRARY_PATH=hdf5/1.8.16/lib')
 
 from rv.structure.Tensor import Matricisation
 
+
 # Base directory of the project
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_MAPPING = {
-    'SDDMX'     :   ['wackylmi-malt-v2-36K.word0.h5', 'wackylmi-malt-v2-36K.word1.h5'],
+    'SDDM'     :   ['wackylmi-malt-v2-36K.word0.h5', 'wackylmi-malt-v2-36K.word1.h5'],
     'TypeDM'    :   ['typedm.matricised.word0.h5'   , 'typedm.matricised.word1.h5']
 }
 
 class MatrixFactory:
     def __init__(self):
         self.matrix = Matricisation({
-            'word0' : os.path.join(BASE_DIR, MODEL_MAPPING['SDDMX'][0]),
-            'word1' : os.path.join(BASE_DIR, MODEL_MAPPING['SDDMX'][1]) 
+            'word0' : os.path.join(BASE_DIR, MODEL_MAPPING['SDDM'][0]),
+            'word1' : os.path.join(BASE_DIR, MODEL_MAPPING['SDDM'][1]) 
         })
+        self.currentModel = 'SDDM'
 
     def setModel(self, modelName):
         if modelName in MODEL_MAPPING.keys():
             fileNameList = MODEL_MAPPING[modelName]
         else:
-            logger.critical( 'errCode: %d. internal error!', errorCode.INTERNAL_ERROR)
-            result = {'errCode' : errorCode.INTERNAL_ERROR}
             return False
         self.matrix.close()
         self.matrix = Matricisation({
             'word0' : os.path.join(BASE_DIR, fileNameList[0]),
             'word1' : os.path.join(BASE_DIR, fileNameList[1]) 
         })
+        self.currentModel = modelName
         return True
 
     def getMatrix(self):
         return self.matrix
+
+    def getCurrentModel(self):
+        return self.currentModel
 
     def __enter__(self):
         return self
