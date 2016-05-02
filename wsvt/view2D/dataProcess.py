f7ad93b813b1e6c5cd3e2c84e80a6511f5354de5
Tony Hong <humanhong1001@gmail.com>
2016-05-02 23:32:16 +0200

    Move mapping functions to mappingSelector module

--- wsvt/view2D/dataProcess.py
+++ wsvt/view2D/dataProcess.py
@@ -14,7 +14,6 @@
         export PYTHONPATH=$PYTHONPATH:view2D
 '''
 
-import math
 import logging
 
 import numpy as np
@@ -23,6 +22,7 @@ import pandas as pd
 from rv.similarity.Similarity import cosine_sim
 
 import errorCode as errorCode
+import mappingSelector as ms
 from matrixFactory import MatrixFactory
 from roleDict import getRoleMapping
 
@@ -182,7 +182,7 @@ def process(verb, noun, role, group, model, topN = 20, quadrant = 4):
             support = 0
             queryFraction = 0
             sumFraction = queryFraction + sumFraction
-        q_x, q_y = mapping(queryFraction, queryCosine, sumFraction, quadrant)
+        q_x, q_y = ms.mapping(queryFraction, queryCosine, sumFraction, quadrant)
 
     for w in wordList:
     # Computer fraction and cosine
@@ -195,9 +195,9 @@ def process(verb, noun, role, group, model, topN = 20, quadrant = 4):
         if inList and w == queryWord1:
             # Apply mapping to second query word
             queryCosine = wordCosine
-            q_x, q_y = mapping(fraction, wordCosine, sumFraction, quadrant)
+            q_x, q_y = ms.mapping(fraction, wordCosine, sumFraction, quadrant)
         else:
-            x, y = mapping(fraction, wordCosine, sumFraction, quadrant)
+            x, y = ms.mapping(fraction, wordCosine, sumFraction, quadrant)
 
             # simularities[w] = wordCosine
             # fractions[w] = fraction
@@ -241,113 +241,3 @@ def getSupport(word0, role, word1):
         return word0.ix[role].ix[word1]
     except KeyError, e:
         return 0
-
-def mapping(fraction, cosine, sumFraction, quadrant = 4):
-    if (quadrant == 1):
-        return mapping_1q(fraction, cosine, sumFraction)
-    elif (quadrant == 4 or quadrant == 32):
-        return mapping_nq(fraction, cosine, sumFraction, quadrant)
-    else:
-        return mapping_sc(fraction, cosine, sumFraction)
-
-def mapping_1q(fraction, cosine, sumFraction):
-    '''
-    Mapping from fraction, and cosine to the x, y coordinate.
-    This is a simple function which maps the high dimension vector to 2D.
-    It is suitable for a web tool which need a short response time.
-
-    @parameters:
-        fraction = support / sumSupport
-        cosine   = cosine_sim(centroid, wordVector)
-    @return: 
-        (x, y) is a tuple
-    '''
-    # Scale fraciton and cosine, let them become more sparse over [0, 1]
-    x = 1 - sumFraction
-    y = 1 - cosine
-
-    # print x, y
-
-    # Compute radial coordinates
-    r = math.sqrt((math.pow(x, 2) + math.pow(y, 2))) * 1.5
-    if x - 0 < 1e-3:
-        rad_b = math.pi / 2
-    else:
-        # Scale rad from [0, pi/2] to [0, 2 * pi]
-        rad_b = math.atan(y / x)
-    rad = rad_b
-
-    # Transform back to Cartesian coordinates
-    x = r * math.cos(rad) - 0.75
-    y = r * math.sin(rad) - 0.75
-
-    return x, y
-
-def mapping_nq(fraction, cosine, sumFraction, quadrant = 4):
-    '''
-    Mapping from fraction, and cosine to the x, y coordinate.
-    This is a simple function which maps the high dimension vector to 2D.
-    It is suitable for a web tool which needs a short response time.
-
-    @parameters:
-        fraction = support / sumSupport
-        cosine   = cosine_sim(centroid, wordVector)
-    @return: 
-        (x, y) is a tuple
-    '''
-    # Scale fraciton and cosine, let them become more sparse over [0, 1]
-    x = 1 - sumFraction
-    y = 1 - cosine
-    weight = 0.5
-
-    # Compute radial coordinates
-    r = math.sqrt(((1 - weight) * math.pow(x, 2) + weight * math.pow(y, 2))/2)
-    
-    rad_b = math.atan2( weight * y, ((1 - weight) * x))
-
-    rad = rad_b * quadrant
-
-    # Transform back to Cartesian coordinates
-    y = r * math.cos(rad)
-    x = r * math.sin(rad)
-
-    return x, y
-
-
-
-
-def mapping_sc(fraction, cosine, sumFraction):
-    '''
-    Mapping from fraction, and cosine to the x, y coordinate.
-    This is a simple function which maps the high dimension vector to 2D.
-    It is suitable for a web tool which need a short response time.
-
-    @parameters:
-        fraction = support / sumSupport
-        cosine   = cosine_sim(centroid, wordVector)
-    @return: 
-        (x, y) is a tuple
-    '''
-    # Scale fraciton and cosine, let them become more sparse over [0, 1]
-    x = 1 - (sumFraction - fraction)
-    y = 1 - cosine
-
-    # print x, y
-
-    # Compute radial coordinates
-    r = x
-    if y - 0 < 1e-3:
-        rad = math.pi / 2
-    else:
-        rad = y * 2 * math.pi + math.pi / 2
-
-    # Transform back to Cartesian coordinates
-    if rad < 2 * math.pi:
-        x = -r * math.cos(rad)
-        y = r * math.sin(rad)
-    else:
-        x = -r * math.cos(rad - 2 * math.pi)
-        y = r * math.sin(rad - 2 * math.pi)
-
-    return x, y
-
---
+++ wsvt/view2D/mappingSelector.py
@@ -0,0 +1,116 @@
+'''
+    mappingSelector.py
+'''
+
+import math
+
+
+def mapping(fraction, cosine, sumFraction, quadrant = 4):
+    if (quadrant == 1):
+        return mapping_1q(fraction, cosine, sumFraction)
+    elif (quadrant == 4 or quadrant == 32):
+        return mapping_nq(fraction, cosine, sumFraction, quadrant)
+    else:
+        return mapping_sc(fraction, cosine, sumFraction)
+
+def mapping_1q(fraction, cosine, sumFraction):
+    '''
+    Mapping from fraction, and cosine to the x, y coordinate.
+    This is a simple function which maps the high dimension vector to 2D.
+    It is suitable for a web tool which need a short response time.
+
+    @parameters:
+        fraction = support / sumSupport
+        cosine   = cosine_sim(centroid, wordVector)
+    @return: 
+        (x, y) is a tuple
+    '''
+    # Scale fraciton and cosine, let them become more sparse over [0, 1]
+    x = 1 - sumFraction
+    y = 1 - cosine
+
+    # print x, y
+
+    # Compute radial coordinates
+    r = math.sqrt((math.pow(x, 2) + math.pow(y, 2))) * 1.5
+    if x - 0 < 1e-3:
+        rad_b = math.pi / 2
+    else:
+        # Scale rad from [0, pi/2] to [0, 2 * pi]
+        rad_b = math.atan(y / x)
+    rad = rad_b
+
+    # Transform back to Cartesian coordinates
+    x = r * math.cos(rad) - 0.75
+    y = r * math.sin(rad) - 0.75
+
+    return x, y
+
+def mapping_nq(fraction, cosine, sumFraction, quadrant = 4):
+    '''
+    Mapping from fraction, and cosine to the x, y coordinate.
+    This is a simple function which maps the high dimension vector to 2D.
+    It is suitable for a web tool which needs a short response time.
+
+    @parameters:
+        fraction = support / sumSupport
+        cosine   = cosine_sim(centroid, wordVector)
+    @return: 
+        (x, y) is a tuple
+    '''
+    # Scale fraciton and cosine, let them become more sparse over [0, 1]
+    x = 1 - sumFraction
+    y = 1 - cosine
+    weight = 0.5
+
+    # Compute radial coordinates
+    r = math.sqrt(((1 - weight) * math.pow(x, 2) + weight * math.pow(y, 2))/2)
+    
+    rad_b = math.atan2( weight * y, ((1 - weight) * x))
+
+    rad = rad_b * quadrant
+
+    # Transform back to Cartesian coordinates
+    y = r * math.cos(rad)
+    x = r * math.sin(rad)
+
+    return x, y
+
+
+
+
+def mapping_sc(fraction, cosine, sumFraction):
+    '''
+    Mapping from fraction, and cosine to the x, y coordinate.
+    This is a simple function which maps the high dimension vector to 2D.
+    It is suitable for a web tool which need a short response time.
+
+    @parameters:
+        fraction = support / sumSupport
+        cosine   = cosine_sim(centroid, wordVector)
+    @return: 
+        (x, y) is a tuple
+    '''
+    # Scale fraciton and cosine, let them become more sparse over [0, 1]
+    x = 1 - (sumFraction - fraction)
+    y = 1 - cosine
+
+    # print x, y
+
+    # Compute radial coordinates
+    r = x
+    if y - 0 < 1e-3:
+        rad = math.pi / 2
+    else:
+        rad = y * 2 * math.pi + math.pi / 2
+
+    # Transform back to Cartesian coordinates
+    if rad < 2 * math.pi:
+        x = -r * math.cos(rad)
+        y = r * math.sin(rad)
+    else:
+        x = -r * math.cos(rad - 2 * math.pi)
+        y = r * math.sin(rad - 2 * math.pi)
+
+    return x, y
+
