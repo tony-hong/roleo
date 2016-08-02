fd766e904890d202ce31f787063369affb51b708
Tony Hong <humanhong1001@gmail.com>
2016-08-02 16:29:45 +0200

    Redefine 1st query word as "selector", 2nd query word as "query word"

--- wsvt/view2D/errorCodeJSON.py
+++ wsvt/view2D/errorCodeJSON.py
@@ -11,13 +11,13 @@ import errorCode
 errorCodeJSON = {
     errorCode.NOUN_FORMAT_ERROR : "Noun format error",
     errorCode.VERB_FORMAT_ERROR : "Verb format error",
-    errorCode.NOUN_EMPTY        : "Noun as primal query word is empty",
-    errorCode.VERB_EMPTY        : "Verb as primal query word is empty",
-    errorCode.MBR_VEC_EMPTY     : "The model return nothing for the primal query word", # MemberVectors is empty, main query word 
-    errorCode.QUERY_EMPTY       : "Second query word does not exist in the model",      # query is empty
-    errorCode.SMT_ROLE_EMPTY    : "Semantic role of second query word does not exist",  # query.ix[semanticRole] is empty
-    errorCode.INTERNAL_ERROR    : "Server internal error, please refresh the page",
+    errorCode.NOUN_EMPTY        : "This noun as a selector does not exist in this model.",
+    errorCode.VERB_EMPTY        : "This verb as a selector does not exist in this model.",
+    errorCode.MBR_VEC_EMPTY     : "The model return nothing for this query. Please enter another one.", # MemberVectors is empty, main query word 
+    errorCode.QUERY_EMPTY       : "The query word does not exist in this model",      # query is empty
+    errorCode.SMT_ROLE_EMPTY    : "Semantic role of the query word does not exist in this model",  # query.ix[semanticRole] is empty
+    errorCode.INTERNAL_ERROR    : "Server internal error. Please refresh the page",
     errorCode.NON_ENGLISH       : "Non english word is temporarily not supported",
-    errorCode.TON_N_ERROR       : "TopN is out of range"
+    errorCode.TON_N_ERROR       : "Top N is out of range"
 
 }
