06a2b63a46cc6491444930eb428a359d1ce95b68
Tony Hong <humanhong1001@gmail.com>
2016-08-09 11:58:00 +0200

    Improve help page regarding LMI

--- wsvt/view2D/static/view2D/js/index.js
+++ wsvt/view2D/static/view2D/js/index.js
@@ -174,8 +174,8 @@ function showExplanation (quadrant) {
     } else if(quadrant == 1){
       setMsgLabel('Y-axis is cosine distance. X-axis is normalized Local Mutual Information.');
     } else {
-      setMsgLabel('Color of a point represents angle. Distance from a point to the centroid is' 
-        + ' combination of cosine distance and normalized Local Mutual Information');
+      setMsgLabel('Distance from a point to the centroid is' 
+        + 'combination of cosine distance and normalized LMI (see Help)');
     }
 }
 
